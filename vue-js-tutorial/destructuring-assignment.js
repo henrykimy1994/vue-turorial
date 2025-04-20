2098c86ffcb60b37a3f5c56196edf36c51c3ac7c
henrykimy1994 <henrykimy.1994@gmail.com>
2025-04-20 21:24:01 +0900

    first commit

---
+++ vue-js-tutorial/destructuring-assignment.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    const person = { name:'Henry', age: 32 }
+    const { name, age } = person
+
+    // Array destructuring
+    const arr = [1, 2 ,3]
+    const [a, b, c] = arr
+    console.log(a);
+    console.log(b);
+    console.log(a);
+
+    const []
+}
